725a62a70db3dae57142f077e47646e7f835f05a
Rusty B <mycoding.alert@gmail.com>
2015-06-20 09:26:23 -0700

    Adding solution and project with first problem solved.
    The project is a unit test project exclusively right now.  Should consider adding an interface at some point that will make looking at solutions easier.

---
+++ 001.MultiplesOf3And5.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Euler
+{
+    [TestClass]
+    public class MultiplesOf3And5Tests
+    {
+        [TestMethod]
+        public void SumAllMultiplesBelow_10_Returns23()
+        {
+            Assert.AreEqual(23, MultiplesOf3And5.SumAllMultiplesBelow(10));
+        }
+
+        [TestMethod]
+        public void SumAllMultiplesBelow_1000_SolvesProblem()
+        {
+            var sum = MultiplesOf3And5.SumAllMultiplesBelow(1000);
+            Assert.AreEqual(233168, sum);
+        }
+    }
+
+    public static class MultiplesOf3And5
+    {
+        /// <summary>
+        /// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+        /// The sum of these multiples is 23.
+        ///
+        /// Find the sum of all the multiples of 3 or 5 below 1000.
+        /// </summary>
+        /// <returns></returns>
+        public static int SumAllMultiplesBelow(int upperBound)
+        {
+            var sum = 0;
+
+            for (var i = 1; i < upperBound; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+            return sum;
+        }
+    }
+}
