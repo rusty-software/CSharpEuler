67c2734658cd6f2955f74fcdd991dc96a2f8ea63
Rusty B <mycoding.alert@gmail.com>
2015-06-20 20:20:45 -0700

    Added IsPrime tests, fixed the implementation

--- 003.LargetPrimeFactor.cs
+++ 003.LargetPrimeFactor.cs
@@ -15,6 +15,32 @@ namespace Euler
             var expected = new List<int> { 5, 7, 13, 29 };
             CollectionAssert.AreEqual(expected, PrimeCalculator.FactorsOf(13195));
         }
+
+        [TestMethod]
+        public void IsPrime_1_IsFalse()
+        {
+            Assert.IsFalse(PrimeCalculator.IsPrime(1));
+        }
+
+        [TestMethod]
+        public void IsPrime_2_IsTrue()
+        {
+            Assert.IsTrue(PrimeCalculator.IsPrime(2));
+        }
+
+        [TestMethod]
+        public void IsPrime_Primes_IsAllTrue()
+        {
+            var primes = new List<int> { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47 };
+            Assert.IsTrue(primes.All(p => PrimeCalculator.IsPrime(p)));
+        }
+
+        [TestMethod]
+        public void IsPrime_NonPrimes_IsAllFalse()
+        {
+            var nonPrimes = new List<int> { 4, 6, 8, 10, 21, 27, 33, 39, 49, 100, 1000 };
+            Assert.IsFalse(nonPrimes.Any(np => PrimeCalculator.IsPrime(np)));
+        }
     }
 
     internal static class PrimeCalculator
@@ -52,7 +78,7 @@ namespace Euler
                 return true;
             }
 
-            for (var i = primes.Last() + 1; i <= num; i++)
+            for (var i = 2; i <= num; i++)
             {
                 if (num % i == 0 && num != i)
                 {
