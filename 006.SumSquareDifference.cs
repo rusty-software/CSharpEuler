2d376ba6122ccd3dbd148553c6c9b5ce955ee85d
Rusty B <mycoding.alert@gmail.com>
2015-06-21 08:57:43 -0700

    Adding sum of squares problem and sum of squares function

---
+++ 006.SumSquareDifference.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Euler
+{
+    [TestClass]
+    public class SquareSumsTests
+    {
+        [TestMethod]
+        public void SumOfSquares_1And10_Returns385()
+        {
+            Assert.AreEqual(385, SquareSums.SumOfSquares(1, 10));
+        }
+    }
+
+    internal static class SquareSums
+    {
+        internal static long SumOfSquares(int lowerBound, int upperBound)
+        {
+            var range = Enumerable.Range(lowerBound, upperBound);
+            var squares = new List<long>();
+            foreach(var i in range)
+            {
+                squares.Add(i * i);
+            }
+            return squares.Sum();
+        }
+    }
+}
