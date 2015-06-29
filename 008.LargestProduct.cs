e711ddac426c464c95a5a0777d9aebd713ec2bff
Rusty B <mycoding.alert@gmail.com>
2015-06-29 16:11:42 -0700

    Started problem 8: Largest product in a series
    Small test, will add the given next.

---
+++ 008.LargestProduct.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace Euler
+{
+    [TestClass]
+    public class DigitAnalyzerTests
+    {
+        [TestMethod]
+        public void LargestProductFromConsecutive_1234567890And4_Returns3024()
+        {
+            var number = 1234567890d;
+            Assert.AreEqual(3024, DigitAnalyzer.LargestProductFromConsecutive(number, 4));
+        }
+    }
+
+    internal static class DigitAnalyzer
+    {
+        internal static double LargestProductFromConsecutive(double number, int count)
+        {
+            var digits = number.ToString().ToCharArray().Select(c => (int)char.GetNumericValue(c)).ToList();
+            var largestProduct = 0d;
+            var upperBound = digits.Count() - count;
+            for (var i = 0; i < upperBound; i++)
+            {
+                var product = Convert.ToDouble(digits.GetRange(i, count).Aggregate((total, next) => total * next));
+                largestProduct = product > largestProduct ? product : largestProduct;
+            }
+            return largestProduct;
+        }
+    }
+}
